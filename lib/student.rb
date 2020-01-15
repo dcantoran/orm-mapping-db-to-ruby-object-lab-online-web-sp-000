20e6ba22fd76f60303be37ed643b3336cf90c39d
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 20:40:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,11 +20,11 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    # sql = <<-SQL
-    #   SELECT * FROM students WHERE grade = 9;
-    # SQL
-    # DB[:conn].execute(sql)
-    self.all.map {|g| g.grade == 9}
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9;
+    SQL
+    DB[:conn].execute(sql)
+    
   end 
   
   def self.students_below_12th_grade
