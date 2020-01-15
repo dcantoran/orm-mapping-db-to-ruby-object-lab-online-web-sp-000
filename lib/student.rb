82d7b47dde4baaea8b55b14ed7fc4e8cd7c06e3d
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 20:33:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 9;
+      SELECT * FROM students WHERE grade < 12;
     SQL
     DB[:conn].execute(sql)
   end 
