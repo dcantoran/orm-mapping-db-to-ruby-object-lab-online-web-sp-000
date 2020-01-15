574e501caf2198e3ab201f83e544e683e3ba03c4
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 20:46:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,12 +28,12 @@ class Student
   end 
   
   def self.students_below_12th_grade
-    # sql = <<-SQL
-    #   SELECT * FROM students WHERE grade < 12
-    # SQL
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map{|row|self.new_from_db(row)}
+    # sql = "SELECT * FROM students WHERE grade < 12"
     # DB[:conn].execute(sql)
-    sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql)
   end 
 
   def self.find_by_name(name)
