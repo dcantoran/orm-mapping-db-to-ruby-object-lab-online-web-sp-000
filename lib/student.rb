206886b97154c1406b17fc057ddf94777cdb4517
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 20:48:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,13 @@ class Student
     SQL
     DB[:conn].execute(sql).map{|row|self.new_from_db(row)}
   end 
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 
+    SQL
+    DB[:conn].execute(sql).map{|row|self.new_from_db(row)}
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
